1c158e649a9328ce916b187f22d2eb08135deb74
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2025-07-05 00:57:11 +0900

    Adding Suggest window

--- kh_lib/kh_sysconfig.pm
+++ kh_lib/kh_sysconfig.pm
@@ -1313,6 +1313,36 @@ sub ClearGeometries{
 	return $self;
 }
 
+sub show_suggest_on_startup{
+	my $self = shift;
+	my $new = shift;
+	
+	if (defined($new)) {
+		$self->{show_suggest_on_startup} = $new;
+	}
+	
+	unless ( defined($self->{show_suggest_on_startup}) ){
+		$self->{show_suggest_on_startup} = 1;
+	}
+	
+	return $self->{show_suggest_on_startup};
+}
+
+sub suggest_stands_with_main{
+	my $self = shift;
+	my $new = shift;
+	
+	if (defined($new)) {
+		$self->{suggest_stands_with_main} = $new;
+	}
+	
+	unless ( defined($self->{suggest_stands_with_main}) ){
+		$self->{suggest_stands_with_main} = 1;
+	}
+	
+	return $self->{suggest_stands_with_main};
+}
+
 sub DocSrch_CutLength{
 	my $self = shift;
 	if (defined($self->{DocSrch_CutLength})){
